6ef9f5b7625c8c0713f6d3c92ebb165cabe75e03
qasim <qasimihsan786@gmail.com>
2025-10-22 23:46:22 -0700

    Modify nav bar

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+console.log (2+3);
+let a=7+5;
+console.log(a);
+const arr= [1,2,3,4,a];
+console.log(arr[1]);
+const stu={name:"qasim",age:12,isGood: true};
+console.log(stu.name);
+console.log(stu.age);
+hobbies:['sketch','gym','fresh'];
+uni:{semester: 5, city:'lhr'};
+if(stu.age!==20){
+console.log("true");
+}
+else{
+    console.log("false");
+}
